c8c0e2d7e6acfafebba668c54369c8ea40a91f30
Shreyansh19-o <dk9806860@gmail.com>
2025-05-13 10:48:09 +0530

    env created and all dependencies installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shreyansh Nayal",
+    author_email="shreyansh19nayal0@gmail.com",
+    packages=find_packages()
+)
